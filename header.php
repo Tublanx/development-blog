52785f712a6cd9e2caf8a23a3ee8ce7de9707166
Tublanx <rlgus03453@naver.com>
2022-12-26 22:23:04 +0900

    main, projects, blog page header layout

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/b17d4fa9e7.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/style/main_style.css">
+    <title>Become a real Developer</title>
+</head>
+<body>
+    <nav class="navbar">
+        <!-- empty div (add if you need logo) -->
+        <div class="navbar_logo">
+        </div>
+
+        <ul class="navbar_menu">
+            <li><a href="/main.php">Home</a></li>
+            <li><a href="/blog.php">Blog</a></li>
+            <li><a href="/projects.php">Projects</a></li>
+        </ul>
+
+        <a href="https://github.com/Tublanx" class="navbar_items">
+            <li><i class="fab fa-github"></i></li>
+        </a>
+
+        <a href="" class="navbar_toggleBtn">
+            <i class="fas fa-bars"></i>
+        </a>
+    </nav>
+</body>
+</html>
