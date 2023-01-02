0ec99ee1c2315a69b55f7134a28cc9eaf8138ade
Tublanx <rlgus03453@naver.com>
2023-01-02 21:52:16 +0900

    해당하는 마크다운 파일을 읽어서 화면에 표시 완료

---
+++ detail.php
@@ -0,0 +1,67 @@
+<?php
+
+require_once 'Michelf/Markdown.inc.php';
+
+include('db_connect.php');
+
+$id = $_GET['id'];
+$query = $pdo->query("select * from post where id='$id';");
+$row = $query->fetch();
+
+$markdown_file = file_get_contents("post/test_$id.md");
+$content = \Michelf\Markdown::defaultTransform($markdown_file);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/b17d4fa9e7.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="/style/header_style.css">
+    <link rel="stylesheet" href="/style/detail_style.css">
+    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+    <title><?php echo $row["title"] ?></title>
+</head>
+
+<body>
+    <?php include('header.php') ?>
+    <div class="container px-4 px-lg-5">
+        <div class="row gx-4 gx-lg-5 justify-content-center">
+            <div class="col-md-10 col-lg-8 col-xl-7">
+                <!-- Post preview-->
+                <div class="post-preview">
+                    <div class="bg">
+                        <h2 class="post-title"><?php echo $row["title"] ?></h2>
+                        <h3 class="post-subtitle"><?php echo $row["subtitle"] ?></h3>
+                    </div>
+                    <p class="post-meta">
+                        Posted by 이기현 on
+                        <?php
+                        $week = array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+                        echo $week[date('w', strtotime($row["createdDate"]))] . " " . date("d Y", strtotime($row["createdDate"]))
+                        ?>
+                    </p>
+                </div>
+                <!-- Divider-->
+                <hr class="my-4" />
+            </div>
+        </div>
+    </div>
+
+    <div class="markdown-content container px-4 px-lg-5">
+        <div class="row gx-4 gx-lg-5 justify-content-center">
+            <div class="col-md-10 col-lg-8 col-xl-7">
+                <?php echo $content ?>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
