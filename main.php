3aeb929fdef13139bc65567923f199fb5dcaecff
Tublanx <rlgus03453@naver.com>
2023-01-04 20:27:05 +0900

    간단한 소개 및 기술들 나열

--- main.php
+++ main.php
@@ -9,78 +9,54 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="/style/header_style.css">
-    <link rel="stylesheet" href="https://unpkg.com/tachyons/css/tachyons.min.css">
+    <link rel="stylesheet" href="style/main_style.css">
     <title>become a s/w developer...</title>
-
-    <style>
-        header {
-            animation: fadein 3s;
-            -moz-animation: fadein 3s;
-            -webkit-animation: fadein 3s;
-            -o-animation: fadein 3s;
-
-            margin-top: 150px;
-        }
-
-        @keyframes fadein {
-            from {
-                opacity: 0;
-            }
-            to {
-                opacity: 1;
-            }
-        }
-
-        @-moz-keyframes fadein {
-            from {
-                opacity: 0;
-            }
-            to {
-                opacity: 1;
-            }
-        }
-
-        @-webkit-keyframes fadein {
-            from {
-                opacity: 0;
-            }
-            to {
-                opacity: 1;
-            }
-        }
-
-        @-o-keyframes fadein {
-            from {
-                opacity: 0;
-            }
-            to {
-                opacity: 1;
-            }
-        }
-    </style>
-
 </head>
 
 <body>
     <?php include('header.php') ?>
-    <main class="measure center tc sans-serif white-80">
-        <div class="flex flex-column justify-center items-center h-100">
-            <header>
-                <h1 class="d2 a-1 f2 fw3">
-                    <code class="db white-40">Developer</code>
-                </h1>
-                <h2 class="d-1 a-2">
-                    I designed it to organize <span class="nowrap">development history</span>
-                </h2>
-                <div class="d-2 mt4">
-                    <a href="https://github.com/Tublanx" class="dib link bw1 b--solid b--dark-blue dark-blue fw9 pa3 br3 hover-white hover-bg-dark-blue">
-                        View on Github
-                    </a>
-                </div>
-            </header>
+    <!-- Masthead-->
+    <header class="masthead text-white text-center">
+        <div class="container d-flex align-items-center flex-column">
+            <!-- Masthead Avatar Image-->
+            <img class="masthead-avatar mb-5" src="img/profile.png" alt="profile" />
+            <!-- Masthead Heading-->
+            <h1 class="masthead-heading text-uppercase mb-0">lee gihyeon</h1>
+            <br>
+            <!-- Masthead Subheading-->
+            <p class="masthead-subheading font-weight-light">Backend Developer</p>
         </div>
-    </main>
+    </header>
+    <!-- Portfolio Section-->
+    <section class="page-section portfolio" id="portfolio">
+        <div class="container">
+            <!-- Portfolio Section Heading-->
+            <h2 class="page-section-heading text-center text-uppercase text-white mb-5">Skills</h2>
+            <!-- Portfolio Grid Items-->
+            <div class="row justify-content-center">
+                <!-- Portfolio Item 1-->
+                <div class="col-md-6 col-lg-4 mb-5">
+                    <div class="portfolio-item" data-bs-toggle="modal" data-bs-target="#portfolioModal1">
+                        <img class="img" width="350px" height="250px" src="img/java.png" alt="java" />
+                    </div>
+                </div>
+                <!-- Portfolio Item 2-->
+                <div class="col-md-6 col-lg-4 mb-5">
+                    <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal2">
+                        <img class="img" width="350px" height="250px" src="img/php.png" alt="php" />
+                    </div>
+                </div>
+                <!-- Portfolio Item 3-->
+                <div class="col-md-6 col-lg-4 mb-5">
+                    <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal3">
+                        <img class="img" width="350px" height="250px" src="img/mysql.png" alt="mysql" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
 
 </html>
