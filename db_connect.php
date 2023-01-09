63dbd283027b98e055550851c24db7676e57ae4b
Tublanx <rlgus03453@naver.com>
2023-01-09 23:32:20 +0900

    더이상 DB로부터 데이터를 안 가져오므로 db_connect 파일 삭제

--- db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db_host = "localhost";
-$db_user = "root";
-$db_password = "1234";
-$db_schema = "blog";
-
-$pdo = new PDO("mysql:host={$db_host};dbname={$db_schema}", $db_user, $db_password);
-?>
